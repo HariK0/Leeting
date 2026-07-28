4a8a35ebc2be97dad5340dca04ff17059cfb7f45
C Hari Kiran <harikiranrocky@gmail.com>
2026-07-28 21:12:10 +0530

    Time: 70 ms (43.3%), Space: 64.3 MB (72.97%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        vector<int> freq(26, 0);
+        int cnt[26] = {0};
 
-        for (char c : s)
-            freq[c - 'a']++;
-
-        string left = "";
-        char mid = '\0';
-
-        for (int i = 0; i < 26; i++) {
-            left.append(freq[i] / 2, char('a' + i));
-            if (freq[i] % 2)
-                mid = char('a' + i);
+        for (char ch : s) {
+            cnt[ch - 'a']++;
         }
 
-        string right = left;
-        reverse(right.begin(), right.end());
+        string firstHalf;
+        firstHalf.reserve(s.size() / 2);
 
-        if (mid != '\0')
-            return left + mid + right;
+        char center = 0;
 
-        return left + right;
+        for (int i = 0; i < 26; i++) {
+            int pairs = cnt[i] / 2;
+            while (pairs--) {
+                firstHalf.push_back('a' + i);
+            }
+
+            if (cnt[i] & 1) {
+                center = 'a' + i;
+            }
+        }
+
+        string result;
+        result.reserve(s.size());
+
+        result += firstHalf;
+
+        if (center) {
+            result.push_back(center);
+        }
+
+        for (int i = static_cast<int>(firstHalf.size()) - 1; i >= 0; i--) {
+            result.push_back(firstHalf[i]);
+        }
+
+        return result;
     }
 };
